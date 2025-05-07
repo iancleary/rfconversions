81fefbd7c76c19ae61bf46beb5359cad68a05828
iancleary <github@iancleary.me>
2025-05-06 22:01:57 -0700

    Add constants.rs

---
+++ src/constants.rs
@@ -0,0 +1,13 @@
+// this is a simplification and assumes in a vacuum
+pub const SPEED_OF_LIGHT: f64 = 299792458.0;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn speed_of_light() {
+        use super::SPEED_OF_LIGHT;
+
+        assert_eq!(299792458.0, SPEED_OF_LIGHT);
+    }
+}
